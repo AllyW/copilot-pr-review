cb3172eb2a3d5af71f82ab97a08dfb1a5f48f862
AllyW <liwang1025@gmail.com>
2024-08-26 23:28:02 +0800

    fix scripts

--- review-scripts.js
+++ review-scripts.js
@@ -42,8 +42,28 @@ const { data } = await octokit.rest.repos.compareCommits({
 
 const { files: changedFiles, commits } = data;
 
-const pathPrompt = 'Below is the diff info from a github pull requests, please make a simple code review, and find the places that can be refined, in a simple and concise way'
-let wholeChange = ''
+const sumPrompt = `Below is the whole changed content from a github pull requests, please draw a summary of this pr with no more than 100 words. Please use the following format:
+PR Summary:
+1. balabala
+2. balabala
+...
+`
+const res = await chat(changedFiles.map(it => it.patch).join('\n'), sumPrompt);
+
+if (!!res) {
+    await octokit.pulls.createReviewComment({
+        owner,
+        repo,
+        pull_number: number,
+        commit_id: commits[commits.length - 1].sha,
+        path: changedFiles[0].filename,
+        body: res,
+        position: 0,
+    });
+}
+
+const pathPrompt = 'Below is the diff info from a github pull requests, please make a simple code review, and find the places that can be refined, in a simple and concise way. Please describe the refinement with no more than 100 words'
+
 for (let i = 0; i < changedFiles.length; i++) {
   const file = changedFiles[i];
   const patch = file.patch || "";
@@ -55,8 +75,6 @@ for (let i = 0; i < changedFiles.length; i++) {
   if (!patch || patch.length > MAX_PATCH_COUNT) {
     continue;
   }
-  wholeChange += patch + '\n';
-
   const res = await chat(patch, pathPrompt);
 
   if (!!res) {
@@ -71,28 +89,6 @@ for (let i = 0; i < changedFiles.length; i++) {
     });
   }
 }
-
-
-const sumPrompt = `Below is the whole changed content from a github pull requests, please draw a summary of this pr with no more than 100 words. Please use the following format:
-PR Summary:
-1. balabala
-2. balabala
-...
-`
-const res = await chat(wholeChange, sumPrompt);
-
-if (!!res) {
-await octokit.pulls.createReviewComment({
-    owner,
-    repo,
-    pull_number: number,
-    commit_id: commits[commits.length - 1].sha,
-    path: changedFiles[0].filename,
-    body: res,
-    position: 0,
-});
-}
-
 
 
 
